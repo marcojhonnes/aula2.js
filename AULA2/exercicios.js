a34a3b7030954ef2af160262efa59fdeda5cad14
marcojhonnes <marcoaurelioguerreiro2012@hotmail.com>
2024-05-27 21:07:37 -0300

    adicionando pasta2

---
+++ AULA2/exercicios.js
@@ -0,0 +1,2 @@
+// Exercício 1: Escreva um programa que recebe dois números como entrada e imprime o maior deles.
+let numero1 = 
---
+++ AULA2/teste.js
@@ -0,0 +1,7 @@
+console.log('a' === 'a')
+console.log(5 === 5)
+console.log(5 == -5)
+console.log(5 === 5)
+console.log(9 > 10)
+
+
